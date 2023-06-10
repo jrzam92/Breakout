1bbbb7133be08bb1a557deedcf6cec50c7fa59b7
jrzam92 <jr_zam92@outlook.com>
2023-06-09 22:28:50 -0600

    se hace cambio de repositorio y tambien se agregan bloques propios para el proyecto

---
+++ Assets/Scripts/Bloque.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloque : MonoBehaviour
+{
+    ///Ideas de porwer up o debuff 
+    ///===Congelamiento de bloque se disminuye la fuerza de bloque 
+    ///===Velocidad a pelota cada vez que toque el bloque 
+    ///===Fuego de bloque aumento de fuerza de bloque
+    ///
+
+    public int resistencia = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (resistencia <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    /// <summary>
+    /// Sobre carga de metodo
+    /// </summary>
+    public virtual void RebotarBola()
+    {
+
+    }
+}
